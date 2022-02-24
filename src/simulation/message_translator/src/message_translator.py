a3b6ff1be1e0a3115ced39d97c31f32f2674ca56
Peng Xu <robotpengxu@gmail.com>
2022-02-24 12:58:35 -0800

    Feat: use tf listener to transform object coordiates. More accuare z value.

--- src/simulation/message_translator/src/message_translator.py
+++ src/simulation/message_translator/src/message_translator.py
@@ -2,6 +2,8 @@
 
 from __future__ import print_function
 
+import numpy as np
+
 # ros
 import rospy
 import tf
@@ -35,11 +37,17 @@ class MessageTranslator(object):
         Variables
         '''
         self.debug = debug
-        self.vehicle_frame_id = self.role_name
+        self.world_frame = 'map'
+        self.vehicle_frame = self.role_name
         self.curr_state = Pose2D()
         self.curr_z = 0
 
         self.detected_objects = None
+
+        '''
+        TF Listener
+        '''
+        self.listener = tf.TransformListener()
 
         '''
         Subscribers
@@ -83,11 +91,11 @@ class MessageTranslator(object):
         self.curr_state.theta = yaw
 
         '''
-        Publish current pose in map frame
+        Publish current pose in world frame
         '''
         self.current_pose = PoseStamped()
         self.current_pose.header.stamp = rospy.get_rostime()
-        self.current_pose.header.frame_id = 'map'
+        self.current_pose.header.frame_id = self.world_frame
         self.current_pose.pose = msg.pose.pose
         self.current_pose_pub.publish(self.current_pose)
 
@@ -103,42 +111,76 @@ class MessageTranslator(object):
         '''
         object_array = ObjectArray()
         object_array.header.stamp = rospy.get_rostime()
-        object_array.header.frame_id = self.vehicle_frame_id
+        object_array.header.frame_id = self.vehicle_frame
 
         for obj in msg.objects:
 
             obj.header.stamp = rospy.get_rostime()
-            obj.header.frame_id = self.vehicle_frame_id
+            obj.header.frame_id = self.vehicle_frame
 
-            # transform quaternion to rpy
-            quaternion = (
-                obj.pose.orientation.x,
-                obj.pose.orientation.y,
-                obj.pose.orientation.z,
-                obj.pose.orientation.w
-            )
-            roll, pitch, yaw = euler_from_quaternion(quaternion)
-            obj_yaw = yaw
+            pose_mat = self.from_pose_msg_to_matrix(obj.pose)
+            transformed = self.world_to_local(pose_mat)
 
-            # transform from global to local
-            p = Point2D(obj.pose.position.x, obj.pose.position.y)
-            p = global_to_local(self.curr_state, self.curr_state.theta, p)
-            theta = obj_yaw - self.curr_state.theta
+            trans = tf.transformations.translation_from_matrix(transformed)
+            quat = tf.transformations.quaternion_from_matrix(transformed)
 
-            obj.pose.position.x = p.x
-            obj.pose.position.y = p.y
-            obj.pose.position.z = obj.pose.position.z - 1.5
+            obj.pose.position.x = trans[0]
+            obj.pose.position.y = trans[1]
+            obj.pose.position.z = trans[2] # + obj.shape.dimensions[2]/2.0
 
-            quaternion = quaternion_from_euler(roll, pitch, theta)
-            obj.pose.orientation.x = quaternion[0]
-            obj.pose.orientation.y = quaternion[1]
-            obj.pose.orientation.z = quaternion[2]
-            obj.pose.orientation.w = quaternion[3]
+            if obj.classification == obj.CLASSIFICATION_CAR:
+                obj.pose.position.z += obj.shape.dimensions[2]/2.0
+
+            obj.pose.orientation.x = quat[0]
+            obj.pose.orientation.y = quat[1]
+            obj.pose.orientation.z = quat[2]
+            obj.pose.orientation.w = quat[3]
 
             object_array.objects.append(obj)
 
         self.detected_object_array_pub.publish(object_array)
 
+    '''
+    Helper functions
+    '''
+    def from_pose_msg_to_matrix(self, pose):
+        trans = tf.transformations.translation_matrix((pose.position.x,
+                                                       pose.position.y,
+                                                       pose.position.z))
+
+        rot = tf.transformations.quaternion_matrix((pose.orientation.x,
+                                                    pose.orientation.y,
+                                                    pose.orientation.z,
+                                                    pose.orientation.w))
+
+        # rospy.loginfo("trans: " + str(trans))
+        # rospy.loginfo("rot: " + str(rot))
+
+        combined = np.matmul(trans, rot)
+        return combined
+
+    def world_to_local(self, pose):
+        """
+        Transform a pose from world frame to local frame
+        @param pose The 4x4 transformation matrix containing the pose to transform
+        @return The 4x4 transformation matrix describing the pose in local frame
+        """
+        # Get pose w.r.t world frame
+        self.listener.waitForTransform(self.vehicle_frame, self.world_frame,
+                                       rospy.Time(), rospy.Duration(10))
+        t, r = self.listener.lookupTransform(self.vehicle_frame, self.world_frame,
+                                             rospy.Time(0))
+
+        # Get relative transform between frames
+        offset = np.matrix(tf.transformations.quaternion_matrix(r))
+        offset[0,3] = t[0]
+        offset[1,3] = t[1]
+        offset[2,3] = t[2]
+
+        # Compose with pose to get pose in local frame
+        result = np.array(np.dot(offset, pose))
+
+        return result
 
 '''
 main function
