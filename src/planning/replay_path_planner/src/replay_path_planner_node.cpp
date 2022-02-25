76b0995234912a82800b8bc2ca325147e9659819
Peng Xu <robotpengxu@gmail.com>
2022-02-24 23:03:11 -0800

    Update replay path planner display.

--- src/planning/replay_path_planner/src/replay_path_planner_node.cpp
+++ src/planning/replay_path_planner/src/replay_path_planner_node.cpp
@@ -8,6 +8,7 @@
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Quaternion.h>
 #include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 #include <casper_auto_msgs/Waypoint.h>
 #include <casper_auto_msgs/WaypointArray.h>
 
@@ -20,6 +21,10 @@ double current_speed;
 double current_z = 0;
 vector<vector<double>> global_route;
 double cruise_speed = 5.0;
+
+double mps2kmph(double velocity_mps) {
+  return (velocity_mps * 60 * 60) / 1000;
+}
 
 void currentPoseCallback(const geometry_msgs::PoseStamped::ConstPtr& msg) {
   ROS_INFO("Received the current_pose ...");
@@ -92,7 +97,7 @@ int main(int argc, char **argv) {
 
   ros::Publisher final_path_pub = n.advertise<nav_msgs::Path>("/final_path", 10);
   ros::Publisher final_waypoints_pub = n.advertise<casper_auto_msgs::WaypointArray>("/final_waypoints", 10);
-  ros::Publisher final_path_marker_pub = n.advertise<visualization_msgs::Marker>("/final_path_marker", 10);
+  ros::Publisher final_path_marker_pub = n.advertise<visualization_msgs::MarkerArray>("/final_path_marker", 10);
 
   ros::Rate rate(60);
 
@@ -151,56 +156,89 @@ int main(int argc, char **argv) {
     ////////////////////////////////////////////////////////////////////////////
     // Construct and Publish generated paths as visualization markers
     ////////////////////////////////////////////////////////////////////////////
-    visualization_msgs::Marker points, line_strip, line_list;
-    points.header.frame_id = line_strip.header.frame_id = line_list.header.frame_id = "map";
-    // points.header.stamp = line_strip.header.stamp = line_list.header.stamp = ros::Time::now();
-    points.ns = line_strip.ns = line_list.ns = "replayed_path";
-    points.action = line_strip.action = line_list.action = visualization_msgs::Marker::ADD;
-    points.pose.orientation.w = line_strip.pose.orientation.w= line_list.pose.orientation.w = 1.0;
+    std_msgs::ColorRGBA waypoint_color;
+    waypoint_color.a = 1.0;
+    waypoint_color.g = 0.7;
+    waypoint_color.b = 1.0;
 
-    points.id = 0;
-    line_strip.id = 1;
-    line_list.id = 1;
+    visualization_msgs::MarkerArray marker_array;
 
-    points.type = visualization_msgs::Marker::POINTS;
-    line_strip.type = visualization_msgs::Marker::LINE_STRIP;
-    line_list.type = visualization_msgs::Marker::LINE_LIST;
+    visualization_msgs::Marker waypoint_marker;
+    waypoint_marker.header.frame_id = "map";
+    waypoint_marker.header.stamp = ros::Time::now();
+    waypoint_marker.ns = "local_path_marker";
+    waypoint_marker.id = 0;
+    waypoint_marker.type = visualization_msgs::Marker::LINE_STRIP;
+    waypoint_marker.action = visualization_msgs::Marker::ADD;
+    waypoint_marker.scale.x = 0.2;
+    waypoint_marker.color = waypoint_color;
+    waypoint_marker.frame_locked = true;
 
-    // POINTS markers use x and y scale for width/height respectively
-    points.scale.x = 0.2;
-    points.scale.y = 0.2;
-
-    // LINE_STRIP/LINE_LIST markers use only the x component of scale, for the line width
-    line_strip.scale.x = 0.1;
-    line_list.scale.x = 0.1;
-
-    // Points are green
-    points.color.g = 1.0f;
-    points.color.a = 1.0;
-
-    // Line strip is blue
-    line_strip.color.b = 1.0;
-    line_strip.color.a = 1.0;
-
-    // Line list is red
-    line_list.color.r = 1.0;
-    line_list.color.a = 1.0;
-
-    // Create the vertices for the points and lines
-    for(int i = 0; i < final_path.size(); i++) {
-      geometry_msgs::Point p;
-      p.x = final_path[i][0];
-      p.y = final_path[i][1];
-      p.z = 0.0;
-
-      points.points.push_back(p);
-      line_strip.points.push_back(p);
-      line_list.points.push_back(p);
-      p.z += 1.0;
-      line_list.points.push_back(p);
+    for (unsigned int i = 0; i < static_cast<int>(final_path.size()); i++)
+    {
+      geometry_msgs::Point point;
+      point.x = final_path[i][0];
+      point.y = final_path[i][1];
+      point.z = current_z + 0.2;
+      waypoint_marker.points.push_back(point);
     }
 
-    final_path_marker_pub.publish(points);
+    marker_array.markers.push_back(waypoint_marker);
+
+    std_msgs::ColorRGBA velocity_color;
+    velocity_color.a = 1.0;
+    velocity_color.g = 0.7;
+    velocity_color.b = 1.0;
+
+    visualization_msgs::Marker velocity_marker;
+    velocity_marker.header.frame_id = "map";
+    velocity_marker.header.stamp = ros::Time::now();
+    velocity_marker.ns = "local_waypoint_velocity";
+    velocity_marker.type = visualization_msgs::Marker::TEXT_VIEW_FACING;
+    velocity_marker.action = visualization_msgs::Marker::ADD;
+    velocity_marker.scale.z = 0.4;
+    velocity_marker.color = velocity_color;
+    velocity_marker.frame_locked = true;
+
+    for (int i = 0; i < static_cast<int>(final_path.size()); i++) {
+      velocity_marker.id = i;
+      velocity_marker.pose.position.x = final_path[i][0];
+      velocity_marker.pose.position.y = final_path[i][1];
+      velocity_marker.pose.position.z = current_z + 0.4;
+
+      // double to string
+      std::ostringstream oss;
+      oss << std::fixed << std::setprecision(1) << mps2kmph(cruise_speed);
+      velocity_marker.text = oss.str();
+
+      marker_array.markers.push_back(velocity_marker);
+    }
+
+    visualization_msgs::Marker waypoint_cube_marker;
+    waypoint_cube_marker.header.frame_id = "map";
+    waypoint_cube_marker.header.stamp = ros::Time::now();
+    waypoint_cube_marker.ns = "local_point_marker";
+    waypoint_cube_marker.id = 0;
+    waypoint_cube_marker.type = visualization_msgs::Marker::CUBE_LIST;
+    waypoint_cube_marker.action = visualization_msgs::Marker::ADD;
+    waypoint_cube_marker.scale.x = 0.2;
+    waypoint_cube_marker.scale.y = 0.2;
+    waypoint_cube_marker.scale.z = 0.2;
+    waypoint_cube_marker.color.r = 1.0;
+    waypoint_cube_marker.color.a = 1.0;
+    waypoint_cube_marker.frame_locked = true;
+
+    for (unsigned int i = 0; i < static_cast<int>(final_path.size()); i++)
+    {
+      geometry_msgs::Point point;
+      point.x = final_path[i][0];
+      point.y = final_path[i][1];
+      point.z = current_z + 0.6;
+      waypoint_cube_marker.points.push_back(point);
+    }
+    marker_array.markers.push_back(waypoint_cube_marker);
+
+    final_path_marker_pub.publish(marker_array);
     // final_path_marker_pub.publish(line_strip);
     // final_path_marker_pub.publish(line_list);
 
